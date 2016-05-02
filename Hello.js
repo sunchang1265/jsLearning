dd5667713e10fcaa3b391693f27e150bbe862b97
sunchang1265 <sunchang1265@gmail.com>
2016-05-02 11:28:32 -0400

    Adding files

---
+++ Hello.js
@@ -0,0 +1,34 @@
+console.log('Node is installed!');
+var http = require('http');
+var server = http.createServer();
+server.on('request', function (request, response) {
+    var headers = request.headers;
+    var method = request.method;
+    var url = request.url;
+    var body = [];
+    request.on('error', function (err) {
+        console.error(err);
+    }).on('data', function (chunk) {
+        body.push(chunk);
+    }).on('end', function () {
+        body = Buffer.concat(body).toString();
+        response.on('error', function (err) {
+            console.error(err);
+        });
+
+        response.statusCode = 200;
+        response.setHeader('content-Type', 'application/json');
+
+        var responseBody = {
+            headers: headers,
+            method: method,
+            url: url,
+            body: body
+        };
+
+        response.write(JSON.stringify(responseBody));
+        response.end();
+    });
+
+
+}).listen(8088);
---
+++ test.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    request.on('error', function (err) {
+        console.error(err);
+        response.statusCode = 400;
+        response.end();
+    });
+    response.on('error', function (err) {
+        console.error(err);
+    });
+    if (request.method === 'GET' && request.url === '/echo') {
+        //    var body = [];
+        //    request.on('data', function(chunk) {
+        //        body.push(chunk);
+        //    }).on('end', function(){
+        //        body = Buffer.concat(body).toString();
+        //        response.end(body);
+        //    });
+        request.pipe(response);
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
+}).listen(8088);
